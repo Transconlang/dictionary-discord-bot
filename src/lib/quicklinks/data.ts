2f5465506868f54ea84366839d2d4c1ee8023596
Akhil Pillai <ackledotdev@gmail.com>
2025-01-07 11:00:29 -0800

    add more sources

--- src/lib/quicklinks/data.ts
+++ src/lib/quicklinks/data.ts
@@ -1,6 +1,31 @@
-import { Resource, ResourceType } from './types';
+import { Resource, ResourceType } from '.';
 
 export const SourcesList: Resource[] = [
+	{
+		name: 'Vocabulary List',
+		url: 'https://github.com/transconlang/translang/tree/main/Vocabulary',
+		type: ResourceType.Specification
+	},
+	{
+		name: 'Grammar',
+		url: 'https://github.com/transconlang/translang/blob/main/Grammar.md',
+		type: ResourceType.Specification
+	},
+	{
+		name: 'Examples',
+		url: 'https://github.com/transconlang/translang/blob/main/Examples.md',
+		type: ResourceType.Specification
+	},
+	{
+		name: 'Obscurisms',
+		url: 'https://github.com/transconlang/translang/blob/main/Obscurisms.md',
+		type: ResourceType.Specification
+	},
+	{
+		name: 'Phonetics',
+		url: 'https://github.com/transconlang/translang/blob/main/Phonetics.md',
+		type: ResourceType.Specification
+	},
 	{
 		name: 'Te Tobacun Kumilinwa "Official"',
 		url: 'https://transconlang.github.io/dictionary-webapp/',
