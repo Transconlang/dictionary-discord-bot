8fb3100c1807dac654f8099544a76ea76dad7fc1
Akhil Pillai <ackledotdev@gmail.com>
2025-07-29 10:34:08 -0700

    error logs

--- src/lib/kumilinwa/refreshCache.ts
+++ src/lib/kumilinwa/refreshCache.ts
@@ -8,7 +8,15 @@ import { fileURLToPath } from 'url';
 const Dirname = join(dirname(fileURLToPath(import.meta.url)));
 
 export async function refreshCachedLangSpec() {
-	const data = await fetch(LangSpecURL).then(res => res.json());
+	const data = await fetch(LangSpecURL).then(res => {
+        try {
+            return res.json();
+        } catch {
+            res.text().then(text => {
+                throw new SyntaxError(`Failed to parse GitHub response body as JSON: ${text}`);
+            });
+        }
+    });
 	await writeFile(join(Dirname, 'langspec.cache.json'), JSON.stringify(data));
 	await new Jsoning(join(Dirname, '..', '..', '..', 'stats.tmp.db.json')).set(
 		'langSpecCacheAge',
